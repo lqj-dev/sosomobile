ccb114ad29e95a5ee2450b35fc401b41625f7ffe
lqj <1724226325@qq.com>
2020-06-22 10:03:38 +0800

    init

---
+++ src/main/java/njpi/soso/model/Student.java
@@ -0,0 +1,10 @@
+package njpi.soso.model;
+
+/**
+ * @Classname Student
+ * @Description TODO
+ * @Date 2020/6/22 9:41 上午
+ * @Created by lqj(1724226325@qq.com)
+ */
+public class Student {
+}
---
+++ src/main/java/njpi/soso/test/Main.java
@@ -0,0 +1,13 @@
+package njpi.soso.test;
+
+/**
+ * @Classname Main
+ * @Description TODO
+ * @Date 2020/6/22 8:55 上午
+ * @Created by lqj(1724226325@qq.com)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
